a36e15c5a119249cf6e47c321cae277f064d51f5
Tom Ivar Helbekkmo <tih@hamartun.priv.no>
2018-05-21 16:32:43 +0200

    Quick hack to let dnsdist daemonize

--- pdns/dnsdist.cc
+++ pdns/dnsdist.cc
@@ -2548,6 +2548,12 @@ try
     }
   }
 
+#ifdef __NetBSD__
+  if(g_cmdLine.beSupervised) {
+    daemon(0, 0);
+  }
+#endif
+
   warnlog("dnsdist %s comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it according to the terms of the GPL version 2", VERSION);
   vector<string> vec;
   std::string acls;
